70ab8e207ef9b292bce610d57bb9290940c0887e
feiyang <feiyangc@outlook.com>
2025-01-23 22:07:18 +0000

    add test separately

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,17 +7,24 @@ from multiprocessing.pool import ThreadPool
 
 @dataclass
 class Foo:
+    def __post_init__(self):
+        self.__increment = 0
+
     @cached_property
     def bar(self):
+        self.__increment += 1
         time.sleep(0.1)
-        return "baz"
+        return self.__increment
 
 
 def test_stdlib_cached_property():
     with ThreadPool(4) as tpool:
         start = time.time()
-        tpool.map(lambda foo: foo.bar, [Foo(), Foo()] * 2)
+        result = tpool.map(lambda foo: foo.bar, [Foo(), Foo()] * 2)
         duration = time.time() - start
 
         if sys.version_info < (3, 12):
             assert (round(duration, 1)) == 0.1
+            assert result == [1, 1, 1, 1]
+        else:
+            assert result == [1, 1, 2, 2]
